14e2392ec5613089188033978431618b992e68f4
Mani Selvam <144051889+Mani-Selvam@users.noreply.github.com>
2025-05-21 00:00:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -415,3 +415,19 @@ window.addEventListener('scroll', () => {
         closeMenu();
     }
 });
+
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+    // Show or hide the button
+    window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+  };
+
+    // Scroll to top when the button is clicked
+    scrollToTopBtn.addEventListener("click", function () {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+  });
